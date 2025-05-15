2612bcd95eea6b5745344b38bfc7e90767972216
kaviya4502 <kaviya0252@gmail.com>
2025-05-15 11:26:46 +0530

    Add files via upload

---
+++ thang.py
@@ -0,0 +1,30 @@
+import mysql.connector
+
+con=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="python_kavi"
+)
+
+
+def insert():
+    name=input("enter your name:")
+    email=input("enter your mail:")
+    city=input("enter your city:")
+
+    res=con.cursor()  
+    sql="insert into user(name,email,city) values (%s,%s,%s)"
+    res.execute(sql,(name,email,city))
+    con.commit()
+    print("data inserted successfully")
+insert()   
+def read():
+    res=con.cursor ()
+    sql="select * from user"
+    res.execute(sql) 
+    result=res.fetchall()
+    print(result)
+read()       
+
+
